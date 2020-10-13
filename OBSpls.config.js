b72afef1ce903829d77c729995cac71aad873fdd
Guido Eckelt <guido.eckelt@gmail.com>
2020-10-14 00:04:04 +0200

    general improvements and delay config addition
    - moved config to a seperate file
    - removed endless loops in selecting a pls and position algorithm
    - added the customization of delays

---
+++ OBSpls.config.js
@@ -0,0 +1,16 @@
+let config = {
+    width : 3,
+    height : 3,
+    maxPls : 4, // DONT MAKE IT A HIGHER NUMBER THAN THERE ARE CELLS, THANKS
+    gifSize : 150,
+    allowSameGifMultipleTimes : false,
+    baseDelay : 5000,
+    allowRandomizedDelay : false,
+    minimumDifference : 2000,
+    maximumDifference : 5000,
+    pls : [
+            "pls/RainbowPls.gif",
+            "pls/BongoBlob.gif",
+            "pls/chikaPls.gif"
+        ]
+};
--- OBSpls.js
+++ OBSpls.js
@@ -1,15 +1,4 @@
-let config = {
-    width: 3,
-    height: 3,
-    maxPls: 4, // DONT MAKE IT THE SAME NUMBER OF CELLS OR MORE, THANKS
-    gifSize : 150,
-    allowSafeGifMultipleTimes: true, // must be true for now
-    pls : [
-            "pls/RainbowPls.gif",
-            "pls/BongoBlob.gif",
-            "pls/chikaPls.gif"
-        ]
-};
+
 let alphabet = "abcdefghijklmnopqrstuvwxyz";
 let container = null;
 
@@ -20,88 +9,80 @@ $(document).ready(function(){
 
 function choosePls(){
     let numberOfPls = 1;
-    if(config.allowSafeGifMultipleTimes){
+    if(config.allowSameGifMultipleTimes){
         numberOfPls = getRandomInt(1, config.maxPls);
     }else{
-        numberOfPls = getRandomInt(1, Math.min(config.pls.length ,config.maxPls));
+        numberOfPls = getRandomInt(1, Math.min(config.pls.length, config.maxPls));
     }
-    console.log("number of Pls " + numberOfPls);
     let selectedPls = new Array();
     for(let i = 0; i < numberOfPls; i++){
-        let pls = selectPls(numberOfPls, selectedPls);
+        let pls = selectPls(selectedPls);
         let position = selectPosition(selectedPls);
         selectedPls.push({ pls: pls, position: position});
     }
     selectedPls.forEach(element => {
         displayPls(element);
     });
-    
-    let delay = getRandomInt(3000, 10000);
+    let delay = config.baseDelay;
+    if(config.allowRandomizedDelay){
+        let minimum = config.baseDelay - config.minimumDifference;
+        let maximum = config.baseDelay + config.maximumDifference;
+        delay = getRandomInt(minimum, maximum);
+    }
     setTimeout( function(){
         container.innerHTML = "";
         choosePls();
     }, delay);
 }
-function selectPls(numberOfPls, selectedPls){
-    let pls = getRandomInt(0, config.pls.length - 1);
-    if(config.allowSafeGifMultipleTimes){
+
+function selectPls(selectedPls){
+    let pls = config.pls[getRandomInt(0, config.pls.length - 1)];
+    if(config.allowSameGifMultipleTimes){
         return pls;
     }
-    // TODO remove endless loop
-    if(selectedPls.length === numberOfPls - 1){
-        selectedPls.forEach(element => {
-            if(element.pls !== pls){
-                return pls;
-            }
-        });
-    }
-    let newPls = true;
+    let possiblePls = Array.from(config.pls);
+    let isPlsNew = true;
     do{
-        
-        newPls = true;
-        console.log("pls " + pls);
+        isPlsNew = true;
+        pls = possiblePls[getRandomInt(0, possiblePls.length - 1)];
         selectedPls.forEach(element => {
-            if(element.pls == pls){
-                console.log("not a new pls");
-                newPls = false;
+            if(element.pls === pls){
+                isPlsNew = false;
+                possiblePls.splice(possiblePls.indexOf(pls), 1);
             }
         });
-        if(newPls === true){
-            return pls;
-        }
     }
-    while(newPls != true);
+    while(isPlsNew === false && possiblePls.length > 0);
+    return pls;
 }
 
 function selectPosition(selectedPls){
     let maxPls = config.width * config.height;
-    let newPosition = true;
-    // TODO remove endless loop
+    let possiblePosition = alphabet.substring(0, maxPls);
+    let position = 0;
+    let isPositionNew = true;
     do{
-        newPosition = true;
-        let temp = alphabet[getRandomInt(0, maxPls - 1)];
-        // console.log("letter " + temp);
+        isPositionNew = true;
+        position = possiblePosition[getRandomInt(0, possiblePosition.length - 1)];
         selectedPls.forEach(element => {
-            if(element.position == temp){
-                console.log("not a new position");
-                newPosition = false;
+            if(element.position === position){
+                isPositionNew = false;
+                possiblePosition = possiblePosition.replace(position, "");
             }
         });
-        if(newPosition === true){
-            return temp;
-        }
     }
-    while(newPosition != true);
+    while(isPositionNew === false && possiblePosition.length > 0);
+    return position;
 }
 
 function displayPls(pls){
     let imgPls = document.createElement("img");
-    imgPls.src = config.pls[pls.pls];
+    imgPls.src = pls.pls;
     imgPls.classList.add("pls-gif");
     imgPls.style = "grid-area: " + pls.position + "; width:" + config.gifSize + "px";
     container.appendChild(imgPls);
 }
 
 function getRandomInt(min, max) {
-    return Math.floor(min + Math.round(Math.random() * max));
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
